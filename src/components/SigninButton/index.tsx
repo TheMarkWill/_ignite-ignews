df9103ac0da74fd4bf2bb6a86f2adf551da5ab18
Marcon Willian <oi@marconwillian.dev>
2021-03-29 19:51:10 -0300

    Fix 🐛: Test export function with JSX.Element

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -4,7 +4,7 @@ import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
-export function SignInButton () {
+export function SignInButton (): JSX.Element {
   const [ session ] = useSession();
 
   console.log(session);
