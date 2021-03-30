60210ad0e2e39f693865a17b3d9be2d6a0c52a19
Marcon Willian <oi@marconwillian.dev>
2021-03-29 21:40:07 -0300

    chore 🎇: Update deploy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,12 +2,13 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 export default NextAuth({
+  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user',
-      authorizationUrl: process.env.GITHUB_CALLBACK
+      scope: 'read:user'
     }),
+    // ...add more providers here
   ],
 })
