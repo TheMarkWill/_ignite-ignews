dbc71dab1c24d1eb7b4bd3c949559367858aa842
Marcon Willian <marcon@b4pay.com.br>
2021-04-01 22:39:15 -0300

    chore 🐛: Add Webhooks

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,6 +1,44 @@
+import { query } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
+
 export async function saveSubscription(
-  subscription: string,
+  subscriptionID: string,
   customerId: string
 ) {
+  try {
+    const userRef = await fauna.query(
+      query.Select(
+        "ref",
+        query.Get(
+          query.Match(
+            query.Index('user_by_stripe_customer_id'),
+            customerId
+          )
+        )
+      )
+    )
   
+    const subscription = await stripe.subscriptions.retrieve(subscriptionID);
+  
+    const subscriptionData = {
+      id: subscription.id,
+      userId: userRef,
+      status: subscription.status,
+      price_id: subscription.items.data[0].price.id
+    };
+  
+    console.log(subscriptionData);
+  
+    await fauna.query(
+      query.Create(
+        query.Collection('subscriptions'),
+        {
+          data: subscriptionData
+        }
+      )
+    )
+  } catch(error){
+    console.log(error)
+  }
 }
