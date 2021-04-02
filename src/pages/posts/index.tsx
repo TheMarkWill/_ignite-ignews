c16965c74a8432aba17c936c744400cb5f53fae4
Marcon Willian <oi@marconwillian.dev>
2021-04-02 12:11:44 -0300

    chore 🎇: Add post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,24 @@
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
 import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -11,36 +27,50 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.postList}>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post em si é este aqui</strong>
-            <p>
-            No React 17 em diante não vamos mais precisar importar o React para apenas usar JSX. A fim de simplificar as coisas, o novo transformador de JSX irá injetar automaticamente as funções necessárias.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post em si é este aqui</strong>
-            <p>
-            No React 17 em diante não vamos mais precisar importar o React para apenas usar JSX. A fim de simplificar as coisas, o novo transformador de JSX irá injetar automaticamente as funções necessárias.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post em si é este aqui</strong>
-            <p>
-            No React 17 em diante não vamos mais precisar importar o React para apenas usar JSX. A fim de simplificar as coisas, o novo transformador de JSX irá injetar automaticamente as funções necessárias.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post em si é este aqui</strong>
-            <p>
-            No React 17 em diante não vamos mais precisar importar o React para apenas usar JSX. A fim de simplificar as coisas, o novo transformador de JSX irá injetar automaticamente as funções necessárias.
-            </p>
-          </a>
+          {posts.map((post: Post) => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => { 
+  
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'post')
+      ],
+      {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100
+      }
+    );
+
+    const posts = response.results.map(post => {
+      return {
+        slug: post.uid,
+        title: RichText.asText(post.data.title),
+        excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? '',
+        updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    }) 
+
+    return {
+      props: {
+        posts
+      }
+    }
 }
