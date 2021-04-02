6577b0fa85037da32145a0d0e8d26e45e3571687
Marcon Willian <oi@marconwillian.dev>
2021-04-02 15:31:27 -0300

    chore ✨: Add link in logo of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,15 @@ import RegisterAndLoginButton from '../SignInButton'
 
 import styles from './styles.module.scss'
 import { ActiveLink } from '../ActiveLink';
+import Link from 'next/link';
 
 export function Header(){
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news"/>
+        <Link href="/">
+          <img src="/images/logo.svg" alt="ig.news"/>
+        </Link>
         <nav>
           <ActiveLink activeClassName={styles.active} href='/' prefetch>
             <a>Home</a>
